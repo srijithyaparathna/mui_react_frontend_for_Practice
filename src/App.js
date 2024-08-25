3911d1a883d2b2ce0a84a05965fc232e400a553c
SKYjithyapa <125755221+SKYjithyapa@users.noreply.github.com>
2024-08-25 21:27:06 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Button, CssBaseline, Typography } from '@mui/material';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AddIcon from '@mui/icons-material/Add';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"  >
+      <CssBaseline /> {/* Apply Material-UI's baseline CSS */}
+      <Button variant="text">Text</Button>
+      <Button startIcon={<SettingsIcon />} variant="contained" color='secondary' size='small'>Contained</Button>
+      <Button startIcon={<AddIcon />} variant="contained" color='success' size='small'>Add new post </Button>
+      <Button variant="outlined" disabled >Outlined</Button>
+      
+
+      <Typography variant="h1" component="h2">
+      it uses h1 style but it is a p tag 
+</Typography>
+<Button variant='contained' sx={{
+        backgroundColor:"skyblue",
+        color:'#888',
+        margin:5
+      }}  > MY Unique Button </Button>
+
+
     </div>
   );
 }
