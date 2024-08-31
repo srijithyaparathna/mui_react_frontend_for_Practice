317178c50ad73f4e06392fedf5639728ba072539
SKYjithyapa <125755221+SKYjithyapa@users.noreply.github.com>
2024-08-31 16:51:22 +0530

    finished practice

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import { Button, CssBaseline, Typography } from '@mui/material';
+import { Button, CssBaseline, styled, Typography } from '@mui/material';
 import SettingsIcon from '@mui/icons-material/Settings';
 import AddIcon from '@mui/icons-material/Add';
+import theme from "./theme"
 
 function App() {
+
+  // create a button as a component when this do we can use this again and again 
+  const BlueButton = styled(Button)(({ theme }) => ({
+   // backgroundColor: theme.palette.otherColor.main,
+   background : "skyblue",
+    color: '#888',
+    margin: 5,
+    "&:hover": {
+      backgroundColor: "lightblue",
+    },
+    "&:disabled": {
+      backgroundColor: "gray",
+      color: "white",
+    },
+  }));
+  
+
+
+
+
+
+
   return (
     <div className="App"  >
       <CssBaseline /> {/* Apply Material-UI's baseline CSS */}
       <Button variant="text">Text</Button>
-      <Button startIcon={<SettingsIcon />} variant="contained" color='secondary' size='small'>Contained</Button>
+      <Button startIcon={<SettingsIcon />} variant="contained" color='otherColor' size='small'>Contained</Button>
       <Button startIcon={<AddIcon />} variant="contained" color='success' size='small'>Add new post </Button>
       <Button variant="outlined" disabled >Outlined</Button>
       
@@ -28,6 +51,21 @@ function App() {
           color:"white"
         }
       }}  > MY Unique Button </Button>
+
+<Button variant='contained' sx={{
+        backgroundColor:"skyblue",
+        color:'#888',
+        margin:5,
+        "&:hover":{
+          backgroundColor:"lightblue"
+        },
+        "&:disabled":{
+          backgroundColor:"gray",
+          color:"white"
+        }
+      }}  > MY Unique Button </Button>
+
+      <BlueButton>Test BlueButton</BlueButton>
 
 
     </div>
